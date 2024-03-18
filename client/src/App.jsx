fc190474ce8f82f4048f0b1e0a7005a94dd863d1
manojvsp <gmanojece2021@gmail.com>
2024-03-18 06:36:04 +0530

    pages ad routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignOut'
 
 const App = () => {
   return (
-    <div className=' text-red-500 font-bold text-center'>App</div>
+   <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signout' element={<SignOut />} />
+     </Routes>
+   </BrowserRouter>
   )
 }
 
