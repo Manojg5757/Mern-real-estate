ee6b315170513da32f2c5750185f0d39c1df5be3
manojvsp <gmanojece2021@gmail.com>
2024-03-19 15:47:47 +0530

    full authentication created with jwt

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js'
+import { signup,signin } from '../controllers/auth.controller.js'
 
 
 const auth = express.Router()
 
 auth.post('/signup',signup)
+auth.post('/signin',signin)
 
 export default auth;
