ee1d3bcf5a30bcbb02e57c46ba6241f1a2fb99fe
manojvsp <gmanojece2021@gmail.com>
2024-03-20 11:25:18 +0530

    saving user details using redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({ user: userSlice });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userSlice },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     return [
       ...getDefaultMiddleware({
         serializableCheck: false,
       }),
-      // Add your custom middleware here, if any
     ];
   },
 });
+
+export const persistor = persistStore(store);
